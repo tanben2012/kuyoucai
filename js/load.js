1d0a831d9cd9bc14d6f80024007626425b054eff
tanben2012 <tanben2012@163.com>
2017-07-06 13:59:07 +0800

    Update load.js

--- js/load.js
+++ js/load.js
@@ -1,10 +1,10 @@
 $(function(){
     //导航定位
     var currenturl = location.href;				//获取到地址栏
-    var re = new RegExp("http://", "g");		//获取到http://
+    var re = new RegExp("https://", "g");		//获取到https://
     currenturl = currenturl.replace(re, "");	//去掉http://
     var currentpagename = currenturl.substring(currenturl.indexOf("/") + 1);	//去掉域名
-	$(".m-nav").find("a[href$='" + currentpagename + "']").addClass("z-crt").siblings().removeClass("z-crt");
+    $(".m-nav").find("a[href$='" + currentpagename + "']").addClass("z-crt").siblings().removeClass("z-crt");
     //侧边栏返回顶部显示状态
     $(window).scroll(function () {
         if ($(window).scrollTop() > 70) {
